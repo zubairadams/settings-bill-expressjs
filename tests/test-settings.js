03cd2d03cfb64c731d511802f80e2dc5c8917ad9
zubairadams <zubair.adams720@gmail.com>
2018-08-28 10:54:35 +0200

    settings

--- tests/test-settings.js
+++ tests/test-settings.js
@@ -2,9 +2,10 @@
 
 let assert = require('assert');
 let settings = require('../settings-bill');
+let setSettings = settings();
 
 describe('the settings bill function', function () {
     it('it should return the value of an sms as the value entered', function () {
-        assert.equal(settings("smsCost"),);
+        assert.equal(1, setSettings.setSettings(1));
     });
 });
